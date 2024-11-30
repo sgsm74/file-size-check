4a30b5f88831cb8f0bf32f6c840871d2f9cc22d9
sgsm74 <saeed.razii93@gmail.com>
2024-12-01 01:25:35 +0330

    Chore: add route

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ use App\Http\Middleware\ValidateContentLength;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return view('welcome');
+});
 Route::post('/upload', [FileUploadController::class, 'uploadFile'])->middleware(ValidateContentLength::class);
