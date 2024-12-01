8812635a27379b8e2d3e5db9b0400a9b4133075d
sgsm74 <saeed.razii93@gmail.com>
2024-12-01 20:57:54 +0330

    Chore: telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Providers\TelescopeServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        $this->app->register(TelescopeServiceProvider::class);
+
     }
 
     /**
